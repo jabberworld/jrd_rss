125d96c724cfe2a44e3651b0b316c3ea0a4c4c4a
rain <rain@gmx.tm>
2022-07-26 17:21:19 +0300

    Version bump: now you can add tags for feeds

--- jrdrss.scheme.sql
+++ jrdrss.scheme.sql
@@ -2,9 +2,11 @@ CREATE TABLE IF NOT EXISTS feeds (
 feedname varchar(255) NOT NULL,
 url varchar(255) NOT NULL,
 description varchar(255) NOT NULL,
+tags varchar(255) DEFAULT NULL;
 subscribers INT NOT NULL DEFAULT 0,
 timeout INT NOT NULL DEFAULT 3600,
 private boolean DEFAULT false,
+registrar varchar(128) DEFAULT NULL,
 regdate TIMESTAMP NOT NULL DEFAULT NOW()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
 
