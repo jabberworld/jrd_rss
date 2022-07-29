3e8b37628482298423f46e7ef934a5a747d620fa
rain <rain@gmx.tm>
2022-07-29 10:23:01 +0300

    Scheme update: utf8->utf8mb4

--- jrdrss.scheme.sql
+++ jrdrss.scheme.sql
@@ -8,7 +8,7 @@ timeout INT NOT NULL DEFAULT 3600,
 private boolean DEFAULT false,
 registrar varchar(128) DEFAULT NULL,
 regdate TIMESTAMP NOT NULL DEFAULT NOW()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
 
 # https://stackoverflow.com/questions/6800866/how-to-store-urls-in-mysql
 # url may be varchar(765)?
@@ -17,11 +17,11 @@ regdate TIMESTAMP NOT NULL DEFAULT NOW()
 CREATE TABLE IF NOT EXISTS subscribers (
 jid varchar(128),
 feedname varchar(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS sent (
 received boolean DEFAULT false,
 md5 varchar(32),
 feedname varchar(255),
 datetime TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
